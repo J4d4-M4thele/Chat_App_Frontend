cb2459407f030fe75072e50fb5c0000cb9d44ea3
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-22 09:57:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from '@mui/material/Container';
-import ChatWindow from './components/ChatWindow';
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Container>
-        <ChatWindow />
+        <Header />
+        <Outlet />
       </Container>
     </div>
   );

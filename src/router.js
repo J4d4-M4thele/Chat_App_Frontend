2ebeb4140eea5d0469fccf40ff11610657251d7c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-22 10:19:24 +0200

    first commit

--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ChatWindow from "./components/ChatWindow";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
+            {
+                path: "/",
+                element: <Home /> 
+            },
             {
                 path: "/chats",
                 element: <ChatWindow/>
